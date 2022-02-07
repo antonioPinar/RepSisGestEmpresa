c6a4834d5cfa4298d99585a13d34d7d8c08a7b0e
AntonioPinar <antoniopinar2@gmail.com>
2022-02-07 22:03:56 +0100

    creado Ej de volcar en un diccionario todos los datos de fich.txt desglosando toda la estructura de directorios

---
+++ EjerciciosDirecYFicheros/direYFicheros2.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+#creamos funcion para introducir en diccionario datos que recuperamos de una estructura de ficheros/directorios
+def crearDicDeFichero():
+    diccNum = {}
+    #abrimos el directorio principal
+    listarDirec = os.listdir("numeros")
+
+    #dentro del directorio principal, descomponemos la estructura hasta llegar al archivo
+    for dir in listarDirec:
+        with open(os.path.join("numeros", dir, dir+".txt")) as f:
+            #leemos los datos que hay en cada fichero
+            lineaDatos = f.readline()
+            listaDatos = lineaDatos.split(",")
+            listaDatos.pop(0)
+
+            #creamos diccionario para limpiar datos de cada fichero, y luego lo añadimos al dicc final
+            dicDatos = {}
+            for dato in listaDatos:
+                datoSeparado = dato.split("=")
+                dicDatos[datoSeparado[0]] = datoSeparado[1]
+
+                #agregamos los datos al diccionario final
+                diccNum[dir] = dicDatos
+    
+    return diccNum
+
+#creamos funcion para borrar toda la estructura del directorio creado
+def borrarDirectorio():
+    shutil.rmtree("numeros")
+
+#ejecutamos 
+print(crearDicDeFichero())
+
+borrarDirectorio()
+print("Estructura de directorios borrada con exito")
