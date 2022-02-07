94c539abd450a45e85e49f431c110b2913f6a85c
AntonioPinar <antoniopinar2@gmail.com>
2022-02-07 19:52:58 +0100

    lineas de codigo añadidas para mostrar num de goles total, tarjetas y espectadores

--- EjerciciosCSV/CSV2.py
+++ EjerciciosCSV/CSV2.py
@@ -13,13 +13,23 @@ for resultado in resultados:
         print(resultado[2])
 
 print()
+
+#variables 
 contL = 0
 contV = 0
 contE = 0
+golesTotales = 0
+tarjetasTotales = 0
+numEspectadorT = 0
+contT = 0
 #si hay mas equipos locales o visitantes que hayan ganado y cuantos de cada uno
 for resultado in resultados[1:]:
     #transformamos el resultado en una lista para poder operar
     operacion = resultado[2].split("-")
+
+    #sumamos el numero total de goles de todos los partidos
+    golesTotales += int(operacion[0]) + int(operacion[1])
+
     #convertimos el string en entero
     if int(operacion[0]) > int(operacion[1]):
         contL += 1
@@ -27,6 +37,15 @@ for resultado in resultados[1:]:
         contV += 1
     else:
         contE += 1
+
+    #contamos numero total de tarjetas en todos los partidos
+    tarjetasTotales += int(resultado[3]) + int(resultado[4])
+
+    #numero total de espectadores en todos los partidos y la media
+    contT += 1
+    numEspectadorT += int(resultado[5])
+
+mediaEspectadores = numEspectadorT / contT
     
 
 if contL > contV:
@@ -34,6 +53,8 @@ if contL > contV:
 else:
     print("Han ganado mas equipos visitantes esta jornada")
 
-print("Hay", contL, "equipos locales que han ganado esta semana")
-print("Hay", contV, "equipos visitantes que han ganado esta semana")
-
+print("Hay", contL, "equipo(s) locales que han ganado esta semana")
+print("Hay", contV, "equipo(s) visitantes que han ganado esta semana")
+print("Esta jornada, ha habido", golesTotales, "goles en total")
+print("Esta jornada, ha habido", tarjetasTotales, "tarjetas en total")
+print("El numero de espectadores totales esta jornada es de", numEspectadorT, "personas, con una media de", mediaEspectadores, "espectadores por partido")
